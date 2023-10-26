1298ce6759958834617fe31c723d13413efbdd5e
Ababaikeli Kahaerzhati <ababaikeli.kahaerzhati@powercoders.org>
2023-10-26 09:35:47 +0200

    Change Addresse

---
+++ Exercise_26-Oct-2023/script.js
@@ -0,0 +1,7 @@
+const question = {
+    question: "What does JSON stand for ?",
+    answer: ["JavaScript Object Notation", "Java Soon Objects Notication", "JavaScript Objects Notation", "JavaScript Object Notification"],
+    corret: 0
+}
+
+const json = JSON.stringify(question);
