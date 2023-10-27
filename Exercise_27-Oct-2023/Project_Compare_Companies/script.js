a1506f4c27636e949f8e763238b6817771cac9d6
Ababaikeli Kahaerzhati <ababaikeli.kahaerzhati@powercoders.org>
2023-10-27 14:15:16 +0200

    Delate Comments

--- Exercise_27-Oct-2023/Project_Compare_Companies/script.js
+++ Exercise_27-Oct-2023/Project_Compare_Companies/script.js
@@ -1,85 +1,9 @@
-// import companies from 'data.json' assert {type: 'json'};
-// import myJson from './data.json' with {type: 'json'};
-/*
-const inputTag_1 = document.querySelector("#input-Company1");
-const inputTag_2 = document.querySelector("#input-Company2");
-const inputTag = document.querySelectorAll("input");
-const formTag = document.querySelector("form");
-const compareBtn = document.querySelector("button");
-/*
-fetch("data.json").then(
-    response => response.json()
-).then(json => {
-    i
-    })
-})
-*/
-/*
-let companies = [];
-
-window.addEventListener("load", async (event) => {
-    let response = await fetch("data.json");
-    companies = await response.json();
-    compareBtn.addEventListener("click", comparison);
-    form.addEventListener("submit", (e)=> {
-        e.preventDefault();
-        comparison();
-    });
-  });
-
-function findCompany(input) {
-
-   return companies.filter(element => element.Name.toLowerCase() === input.value.toLowerCase());
-    
-}
-
-function comparison () {
-    const compan_1_Info = findCompany(inputTag_1);
-    const compan_2_Info = findCompany(inputTag_2);
-    console.log("hello");
-    console.log(compan_1_Info);
-    console.log(compan_2_Info);
-
-}
-
-
-const tableTag = document.querySelector("table");
-
-function creatTable(Obj) {
-    const trTag = document.createElement("tr");
-    const thTag = document.createElement("th");
-    const tdTag = document.createElement("td");
-    for(let element in Obj) {
-        thTag.textContent = element;
-        tdTag.textContent = Obj[element];
-    }
-    
-    tableTag.appendChild(trTag);
-    trTag.appendChild(thTag);
-    trTag.appendChild(tdTag);
-}
-
-*/
-
-// import companies from 'data.json' assert {type: 'json'};
-// import myJson from './data.json' with {type: 'json'};
-
 const inputTag_1 = document.querySelector("#input-company1");
 const inputTag_2 = document.querySelector("#input-company2");
 const inputTag = document.querySelectorAll("input");
 const compareBtn = document.querySelector("button");
 
 const formTag = document.querySelector("form");
-/*
-fetch("data.json").then(
-    response => response.json()
-).then(json => {
-    inputTag.addEventListener("submit", (e)=> {
-        e.preventDefault();
-        comparison();
-    })
-})
-*/
 
 let companies = [];
 
